cbc8173e171694a85ee6251382d3bbbb92cb71c4
김젼이 <97428310+Kimjyunny@users.noreply.github.com>
2022-12-28 14:22:50 +0900

    Add files via upload

---
+++ router/src/pages/Menubar.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link, Outlet, useNavigate } from "react-router-dom";
+
+const Menubar = () => {
+  //Navigate 사용하기
+  const navigate = useNavigate();
+  const goHome = () => {
+    navigate("/");
+  };
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/movies">Movies</Link>
+        </li>
+      </ul>
+
+      <Outlet />
+
+      <button onClick={goHome}>홈으로 돌아가기</button>
+    </div>
+  );
+};
+
+export default Menubar;
